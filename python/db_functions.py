85c6bfe2c2c364a5f9789bd7c07455b8da1dd0dd
Sam Kolovson <skolovso@student.umass.edu>
2014-07-29 11:20:32 -0400

    Add mysql backup and restore in python.

--- python/db_functions.py
+++ python/db_functions.py
@@ -5,10 +5,24 @@ cursor = connection.cursor()
 
 # Create database with named db_name
 def create(cursor, db_name):
-	return cursor.execute("CREATE DATABASE `%s`" % (db_name))
+    return cursor.execute("CREATE DATABASE `%s`" % (db_name))
 
 # Check if database exists
 def exists(cursor, db_name):
-	sql = "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '%s'" % (db_name)
-	return cursor.execute(sql)
+    sql = "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '%s'" % (db_name)
+    return cursor.execute(sql)
+
+def backup_db(db_name):
+    # mysqldump db_name > db_name.sql
+    system("mysqldump %s > %s.sql" % (db_name, db_name)) 
+
+    # multiple databases
+    # mysqldump --databases people places things > nouns.sql
+
+    # all databases
+    # mysqldump --all-databases > my_database.sql
+    
+def restore_db(db_name):
+    # mysql db_name < db_name.sql
+    system("mysql %s > %s.sql" % (db_name, db_name))
 
