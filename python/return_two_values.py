411c1b6f9f0569eedfd78bf01f519cc680e4a3c8
Sam Kolovson <samantha@pointed.com>
2014-08-11 11:28:59 -0400

    Add python example function for returning two values.

---
+++ python/return_two_values.py
@@ -0,0 +1,8 @@
+# How to return two values as a tuple/list
+# http://stackoverflow.com/questions/9752958/how-can-i-return-two-values-from-a-function-in-python
+
+def return_two():
+    # function operations
+    return (one, two)
+
+my_one, my_two = return_two()
